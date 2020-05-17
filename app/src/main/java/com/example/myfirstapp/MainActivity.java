66997a9d2d04d0775d7d0209f3fe20245041b678
Daniele Monaca <daniele.monaca1@gmail.com>
2020-05-17 18:50:57 +0200

    Just a starting try

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,28 +3,54 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.audiofx.DynamicsProcessing;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    private EditText insertBirthDate;
+    private Button button;
+    private TextView showAge;
+
+    private String ageSaved;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        insertBirthDate = findViewById(R.id.insertAge);
+        button = findViewById(R.id.sendBtn);
+        showAge= findViewById(R.id.showAge);
+
+
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                //cosa fare al tocco del click
+                ageSaved =String.valueOf(insertBirthDate.getText());
+                Log.d("MainActivity", String.valueOf(computeAge(Integer.getInteger(ageSaved))));
+                showAge.setText(String.valueOf(computeAge(Integer.getInteger(ageSaved))));
+            }
+        });
+
     }
 
-    /** Should be called when the user press the button*/
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    private int computeAge (int birthDate ){
+        return (Calendar.getInstance().getTime().getYear())-birthDate;
     }
 
 }
